ea8fa780cd519ae4ab7d313e47a27867a44997af
Bene Lemuel <blemuel@gmail.com>
2023-06-25 14:14:15 -0300

    auth routes

--- routes/api.php
+++ routes/api.php
@@ -6,11 +6,11 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\EventController;
 use App\Http\Controllers\UserController;
 
-Route::group(['middleware' => ['auth:sanctum']], function(){
-    Route::post('/event', 'EventController@create');
-    Route::post('/attend/{event}', 'EventController@attend');
-    Route::get('/user', 'UserController@index');
+Route::group(["middleware" => ["auth:sanctum"]], function () {
+    Route::post("/event", "EventController@create");
+    Route::post("/attend/{event}", "EventController@attend");
+    Route::get("/user", "UserController@index");
 });
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+Route::post("/register", [AuthController::class, "register"]);
+Route::post("/login", [AuthController::class, "login"]);
